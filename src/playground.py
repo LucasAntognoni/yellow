0968ce97b06b6a0742bbac5d57c03d1e6372505c
Lucas Antognoni <antognoni.lucas@gmail.com>
2019-06-19 00:20:45 -0300

    Dev enviroment set and project init

---
+++ src/playground.py
@@ -0,0 +1,36 @@
+import requests
+from google.cloud import storage
+
+
+def test_requests(email, token):
+
+    headers = {'Auth': email + ':' + token}
+    data = {"name": "", "brand": "","price": 0}
+
+    # response = requests.get('https://price-api-psel.raccoon.ag/prices', headers=headers)
+    # response = requests.get('https://product-api-psel.raccoon.ag/categories', headers=headers)
+    # response = requests.post('https://product-api-psel.raccoon.ag/categories/0/products', headers=headers, json=data)
+
+    print('Status: %s' % response.status_code)
+    
+    for c in response.json():
+        print(c)
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    
+    storage_client = storage.Client.from_service_account_json('../authentication/account.json')
+
+    # Make an authenticated API request
+    # buckets = list(storage_client.list_buckets())
+    # print(buckets)
+
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print('Blob {} downloaded to {}.'.format(
+        source_blob_name,
+        destination_file_name))
+
+# test_requests()
+download_blob('catalog-store', 'catalog.sample.csv', 'dl_catalog.sample.csv')
